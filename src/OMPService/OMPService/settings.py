5ccddc2ca477295dbe141b6be4fd765fcb921662
admin <sunyaxiong>
2018-05-23 13:24:41 +0800

    nginx reload

--- src/OMPService/OMPService/settings.py
+++ src/OMPService/OMPService/settings.py
@@ -66,7 +66,7 @@ AUTHENTICATION_BACKENDS = (
 )
 
 # cas conf
-SUCC_REDIRECT_URL = "cmp.ecscloud.com:8081"
+SUCC_REDIRECT_URL = "itsm.ecscloud.com"
 CAS_SERVER_URL = "http://52.83.173.240:8080/cas/"
 # CAS_REDIRECT_URL = "http://www.baidu.com"
 
